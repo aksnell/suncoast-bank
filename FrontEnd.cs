4dccc90d8e209daad43841a50a7eb32eb9344020
Alex Snell <alexksnell@gmail.com>
2020-05-21 23:33:11 -0400

    Adventure mode complete

--- FrontEnd.cs
+++
@@ -1,6 +0,0 @@
-namespace SuncoastBank
-{
-    public class FrontEnd
-    {
-    }
-}
--- Transaction.cs
+++ Transaction.cs
@@ -2,5 +2,9 @@ namespace SuncoastBank
 {
     public class Transaction
     {
+        public string Name            { get; set; }
+        public string AccountType     { get; set; }
+        public string TransactionType { get; set; }
+        public int AccountDelta       { get; set; }
     }
 }
